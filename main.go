d1421dc04a7fd1dfdb1c354595b2e96766a65968
Jeff Palm <jeff@jeffpalm.com>
2022-04-29 08:50:07 -0400

    improve HTML a little???

--- main.go
+++ main.go
@@ -28,6 +28,7 @@ var (
 	cacheDir  = flag.String("cache_dir", ".cache", "directory of the cache")
 	timeout   = flag.Duration("timeout", 3*time.Second, "timeout for contacting hosts")
 	outHTML   = flag.String("out_html", "", "output HTML file")
+	test      = flag.Bool("test", false, "use test subdomains")
 
 	colorRE = regexp.MustCompile(`\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]`)
 	hostRE  = regexp.MustCompile(`^[a-zA-Z0-9\-_]+(?:\.[a-zA-Z0-9\-_]+)+$`)
@@ -43,13 +44,15 @@ func outputHTML(subdomains []string) {
 	<table style="width: 100%; height: 100%">
 		<tr>
 			<td style="width: 20%">
-				<ul style="list-style:none">
-				{{range .Subdomains}}
-					<li>
-						<a href="#" data-href="{{.}}" onclick="document.getElementById('iframe').src='http://{{.}}';">{{.}}</a>
-					</li>
-				{{end}}
-				</ul>
+				<div style="height:100%; overflow:auto">
+					<ul style="list-style:none">
+					{{range .Subdomains}}
+						<li>
+							<a href="#" data-href="{{.}}" onclick="document.getElementById('iframe').src='http://{{.}}';">{{.}}</a>
+						</li>
+					{{end}}
+					</ul>
+				</div>
 			</td>
 			<td style="width: 80%">
 				<iframe id="iframe" src="" style="border: 0; width: 100%; height: 100%"></iframe>
@@ -86,18 +89,25 @@ func renderTemplate(t string, name string, data interface{}) ([]byte, error) {
 
 func findSubdomains() (chan string, int) {
 	res := make(chan string)
-
-	cmd := exec.Command("python", *sublist3r, "-d", *host)
-	var stdout bytes.Buffer
-	cmd.Stdout = &stdout
-	check.Err(cmd.Run())
-
 	var subdomains []string
-	for _, h := range slice.Strings(stdout.String(), "\n") {
-		if h := strings.TrimSpace(colorRE.ReplaceAllString(h, "")); hostRE.MatchString(h) {
-			subdomains = append(subdomains, h)
+
+	if *test {
+		subdomains = []string{
+			"foo.com",
+			"bar.com",
+		}
+	} else {
+		cmd := exec.Command("python", *sublist3r, "-d", *host)
+		var stdout bytes.Buffer
+		cmd.Stdout = &stdout
+		check.Err(cmd.Run())
+		for _, h := range slice.Strings(stdout.String(), "\n") {
+			if h := strings.TrimSpace(colorRE.ReplaceAllString(h, "")); hostRE.MatchString(h) {
+				subdomains = append(subdomains, h)
+			}
 		}
 	}
+
 	go func() {
 		for _, h := range subdomains {
 			res <- h
